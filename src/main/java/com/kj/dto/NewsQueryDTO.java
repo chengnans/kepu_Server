12d688166816f53a060a4f15dfc9c4e02e213b2b
chengnan <chengnans@outlook.com>
2024-07-29 11:08:17 +0800

    补全

---
+++ src/main/java/com/kj/dto/NewsQueryDTO.java
@@ -0,0 +1,35 @@
+package com.kj.dto;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author 破晓
+ * @date 2022-01-09 15:36
+ *
+ * 查询新闻主体
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewsQueryDTO {
+
+    private Integer id;
+
+    private String title;
+
+    private Date releaseTime;
+
+    private Integer hits;
+
+    private String content;
+    //author和source为后加
+    private String author;
+    private String sources;
+}
